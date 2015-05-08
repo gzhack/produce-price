1c3714792bbf1906d9b6c41e711ed37d881b51d2
Mr-Wiredancer <lijiahao90@gmail.com>
2015-05-08 10:40:18 +0800

    updated crawler and data

--- crawler.js
+++ crawler.js
@@ -1,8 +1,13 @@
 var _ = require('lodash');
-var jsdom = require('jsdom');
 var async = require('async');
-var gbk = require('gbk');
 var fs = require('fs');
+var iconv = require('iconv-lite');
+var request = require('request');
+var jsdom = require('jsdom');
+
+require('date-utils');
+
+var PAGE_PATTERN = /(\(1\/)([^]*?)(\)页，)/;
 
 var schema = {
   "农产品名称": "table.Tab_mar > tbody > tr > td:nth-child(1) > a:nth-child(1).text_9pt000000",
@@ -13,11 +18,36 @@ var schema = {
   "日期": "td.Ntab_RPad10px > table.Tab_mar > tbody > tr:nth-child(1n + 2) > td:nth-child(6):not(.kim-table-normalizer)"
 };
 
+var betterRequest = function(config, callback) {
+  config.encoding = null; // 为了让 request 返回 buffer 而不是直接对 buffer 解码
+  request(config, function(err, res, body) {
+    if (err || res.statusCode!=200) return callback(err || new Error(res.statusCode));
+    
+    var isGBK = res.headers['content-type'].toUpperCase().indexOf('GBK')>-1;
+
+    callback(null, isGBK?iconv.decode(body, 'GBK'):body.toString());
+  });
+}
+
+var current = new Date(2011, 0, 1); //2008年10月29日
+
+var today = Date.today();
+
+var forwardCurrent = function() {
+  current.setDate(current.getDate()+1);
+};
+
+var getLastDay = function(date) {
+  var lastDay = date.clone();
+  lastDay.setDate(lastDay.getDate()-1);
+  return lastDay.toYMD();
+}
+
 var keys = _.keys(schema);
 
 // 该数据有 50000+ 页，生成 URL
 var urlGenerator = function(pageNum) {
-  return "http://www.abuya.com.cn/abuya/http/Pri_detail.jsp?pageNum=" + pageNum + "&type=&market_id=&area_type=1&search_name=&type_id=";
+  return "http://www.abuya.com.cn/abuya/http/Pri_detail.jsp?pageNum=" + pageNum;
 };
 
 var mapping = {
@@ -30,7 +60,7 @@ var mapping = {
 };
 
 // 在 HTML 页面显示时因字符串太长而省略，此处补全
-var transform = function (data, callback) {n
+var transform = function (data, callback) {
   callback(null, data.map(function(row){
     var original = row['市场名称'];
     var long = mapping[original];
@@ -39,88 +69,123 @@ var transform = function (data, callback) {n
   }));
 };
 
-var crawlHelper = function(pageNumTotal, cb) {
-  async.mapLimit(_.range(1, pageNumTotal+1), 5, crawl, function(err, results) {
-    if (err) return cb(err);
-    cb(null, _.flatten(results));
+var isExist = function(str) {
+  try{
+    var fd = fs.openSync('./data/'+str, 'r');
+    fs.closeSync(fd);
+    return true;
+  }catch(e) {
+    return false;
+  }
+};
+
+var getPageNumTotal = function(html, cb) {
+  jsdom.env(html, function(errors, window) {
+    if (errors) return cb(errors);
+
+    var document = window.document;
+
+    try{
+      var numText = document.querySelector('td.Ntab_RPad10px > table.Tab_mar > tbody > tr:nth-child(22) > td:nth-child(1)').textContent.match(PAGE_PATTERN)[2];
+    } catch(e) {
+      return cb(null, 0);
+    }
+
+    window.close();
+
+    return cb(null, Number(numText));
   });
 };
 
-var crawl = function(pageNum, cb) {
-  var url = urlGenerator(pageNum);
+async.doWhilst(
+  function(callbackDateFinished) {
+    var currentDate = current.toYMD();
 
-  gbk.fetch(url).to('string', function(err, str) {
-    if (err) return cb(err);
+    if (isExist(currentDate)) return callbackDateFinished(null);
 
-    jsdom.env(str, function(errors, window) {
-      if (errors) return cb(errors);
+    console.log('doing '+currentDate);
 
-      var document = window.document;
+    async.waterfall([
 
-      var result = {};
+      // get html to parse total page number
+      function(cb) {
+        var config = {
+          url: urlGenerator(1),
+          method: 'POST',
+          form: {
+            begin_time: getLastDay(current),
+            end_time: currentDate
+          }
+        };
 
-      _.forEach(schema, function(selector, key) {
+        betterRequest(config, cb);
+      }, 
+      getPageNumTotal, //parse page number
+      function(pageNumTotal, cb) {
+        async.mapSeries(_.range(1, pageNumTotal+1), function(pageNum, callbackPageFinished) {
 
-        result[key] = _.map(document.querySelectorAll(selector), function(ele) {
-          return ele.textContent;
-        });
+          console.log(pageNum+'/'+pageNumTotal);
+          var config = {
+            url: urlGenerator(pageNum),
+            method: 'POST',
+            form: {
+              begin_time: getLastDay(current),
+              end_time: currentDate
+            }
+          };
+
+          betterRequest(config, function(err, html) {
+            if (err) return callbackPageFinished(err);
+
+
+            jsdom.env(html, function(errors, window) {
+              if (errors) return cb(errors);
+
+              var document = window.document;
+
+              var result = {};
+
+              _.forEach(schema, function(selector, key) {
+
+                result[key] = _.map(document.querySelectorAll(selector), function(ele) {
+                  return ele.textContent;
+                });
+              });
+
+              window.close();
+              if (process.memoryUsage().heapUsed > 200000000) { 
+                //only call if memory use is bove 200MB
+                global.gc();
+              }
+
+              return callbackPageFinished(null, _.map(result[keys[0]], function(val, index) {
+                var row = {};
+                _.forEach(keys, function(key) {
+                  row[key] = result[key][index];
+                });
+                return row;
+              }));
+            });
+          });
+
+        }, cb);
+      }, function(arr, cb) {
+        var arr = _.flatten(arr);
+
+        fs.writeFile('./data/'+current.toYMD(), 
+          _.map(arr, function(item) {
+            return _.map(keys, function(key) {
+              return JSON.stringify(item[key]);
+            });
+          }).join('\n'), cb);
+      }], function(err) {
+        if (err) return console.log(err);
+        callbackDateFinished(null);
       });
-
-      window.close();
-      if (process.memoryUsage().heapUsed > 200000000) { 
-        //only call if memory use is bove 200MB
-        global.gc();
-      }
-
-      console.log('finished page: '+pageNum);
-
-      return cb(null, _.map(result[keys[0]], function(val, index) {
-        var row = {};
-        _.forEach(keys, function(key) {
-          row[key] = result[key][index];
-        });
-        return row;
-      }));
-    });
-  });
-}
-
-var getPageNumTotal = function(callback) {
-  var pattern = /(\(1\/)([^]*?)(\)页，)/;
-
-  gbk.fetch(urlGenerator(1)).to('string', function(err, str) {
-    if (err) return callback(err);
-
-    jsdom.env(str, function(errors, window) {
-      if (errors) return callback(errors);
-
-      var document = window.document;
-
-      var numText = document.querySelector('td.Ntab_RPad10px > table.Tab_mar > tbody > tr:nth-child(22) > td:nth-child(1)').textContent.match(pattern)[2];
-
-      console.log(numText);
-
-      return callback(null, Number(numText));
-    });
-  });
-}
-
-async.waterfall(
-  [
-    getPageNumTotal,
-    crawlHelper,
-    transform
-  ], function(err, data) {
-    if (err) return console.dir(err);
-
-    var result = {
-      timeUpdated: Date.now(),
-      count: data.length,
-      results: data
-    };   
-
-    fs.writeFile('data.json', JSON.stringify(result, null, 2), function(err) {
-      if (err) return console.dir(err);
-      console.log('success');
-    });
+  }, 
+  function() {
+    forwardCurrent();
+    return current.isBefore(today);
+  }, function(err) {
+    console.dir(err);
   });
